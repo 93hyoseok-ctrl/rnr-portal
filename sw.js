26aa551f11512d0a6d9b245bd4d0d4b1a2b78c5a
93hyoseok-ctrl <93hyoseok@gmail.com>
2026-05-26 08:28:28 +0900

    Service Worker 캐시 제거

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE = 'rnr-portal-v2'
-const ASSETS = ['/', '/index.html', '/manifest.json']
+const CACHE = 'rnr-portal-v3'
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
   self.skipWaiting()
 })
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ))
   self.clients.claim()
 })
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  )
+  e.respondWith(fetch(e.request))
 })
